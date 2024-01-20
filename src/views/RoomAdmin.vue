a83f77e7a8aad84225c521137fe29603e429636a
TheElderLord <61541224+TheElderLord@users.noreply.github.com>
2024-01-20 21:59:57 +0600

    working with vcalendar

--- src/views/RoomAdmin.vue
+++ src/views/RoomAdmin.vue
@@ -1,133 +1,128 @@
-<script>
+<script setup>
 import axios from "axios";
-import { ref, onMounted } from 'vue';
-import { useScreens } from 'vue-screen-utils';
-export default {
-  props: ["id"],
-  name: "room-admin",
-  data() {
-    const { mapCurrent } = useScreens({ xs: '0px', sm: '640px', md: '768px', lg: '1024px' });
-    const columns = mapCurrent({ lg: 2 }, 1);
-    return {
-      info: "",
-      images: [],
+import { ref, onMounted, watch, defineProps } from "vue";
+import { useScreens } from "vue-screen-utils";
 
-      date: null,
-      bookingDate: null,
 
-      columns
-    };
-  },
-  setup() {
-    const { mapCurrent } = useScreens({
-  xs: '0px',
-  sm: '640px',
-  md: '768px',
-  lg: '1024px',
+const props = defineProps(["id"]);
+
+const info = ref("");
+const bookHistory = ref("");
+const images = ref([]);
+
+
+const date = ref(null);
+
+
+const bookingDate = ref(null);
+
+const { mapCurrent } = useScreens({
+  xs: "0px",
+  sm: "640px",
+  md: "768px",
+  lg: "1024px",
 });
-
-const columns = ref(mapCurrent({ lg: 2 }, 1));
+const columns = ref(mapCurrent({ lg: 4 }, 1));
 const expanded = ref(mapCurrent({ lg: false }, true));
 
-const isCurrentMonth = (date) => {
-  // Check if the date is in the current month
-  const currentDate = new Date();
-  return date.getMonth() === currentDate.getMonth();
+
+
+const attributes = ref([]);
+const selectedAttribute = ref(null);
+
+const handleDayClick = (date, attribute) => {
+  console.log("Day Clicked:", date);
+  console.log("Selected Attribute:", attribute);
+  selectedAttribute.value = attribute;
 };
 
-const formatMonth = (date) => {
-  // Format the month as needed
-  const options = { month: 'long' };
-  return date.toLocaleDateString('en-US', options);
+const getInfo = async () => {
+  try {
+    const { data } = await axios.get(
+      `http://localhost:3000/api/v1/rooms/${props.id}`
+    );
+    // console.log(data);
+    info.value = data.items[0];
+    info.value.images = info.value.images.split(",");
+    
+    getBookHistory();
+   
+  } catch (error) {
+    console.error("Error getting room info:", error);
+  }
 };
-  },
-  methods: {
-    goBack() {
-      this.$router.go(-1);
-    },
-    async getInfo() {
-      const data = await axios.get(
-        `http://localhost:3000/api/v1/rooms/${this.id}`
-      );
-      this.info = data.data.items[0];
-      this.info.images = this.info.images.split(",");
-      this.info.booked_date = this.info.booked_date.split("-");
-    },
-    // handleDate([startDate, endDate]) {
-    //   const options = { year: "numeric", month: "2-digit", day: "2-digit" };
-    //   const first = new Date(startDate).toLocaleDateString(undefined, options);
-    //   const second = new Date(endDate).toLocaleDateString(undefined, options);
-    //   this.bookingDate = first + "-" + second;
-    //   console.log(this.bookingDate);
-    //   // do something else with the data
-    // },
-    async bookRoom(id) {
-      if (!this.bookingDate) {
-        alert("Need a booking date");
-        return;
-      }
-      const result = await axios.patch(
-        `http://localhost:3000/api/v1/rooms/${id}`,
-        {
-          bookDate: this.bookingDate,
-        }
-      );
-      this.rooms.find((e) => e.id == id).status = "booked";
-      this.rooms.find((e) => e.id == id).booked_date = this.bookingDate;
-      this.bookingDate = null;
-      console.log(result);
-    },
-    async setFree(id) {
-      const result = await axios.put(
-        `http://localhost:3000/api/v1/rooms/${id}`
-      );
-      this.rooms.find((e) => e.id == id).status = "free";
-      this.rooms.find((e) => e.id == id).booked_date = "";
-      console.log(result);
-    },
-    // formatDate(date) {
-    //   const yyyy = date.getFullYear();
-    //   const mm = String(date.getMonth() + 1).padStart(2, "0");
-    //   const dd = String(date.getDate()).padStart(2, "0");
-    //   return `${yyyy}-${mm}-${dd}`;
-    // },
 
-    // async initDate() {
-    //   const startDate = new Date();
-    //   const endDate = new Date(new Date().setDate(startDate.getDate() + 7));
-    //   this.date = [startDate, endDate];
-    // },
-  },
- 
-  mounted() {
-    console.log(this.id); // Make sure the id is correctly logged
-    this.getInfo();
-    // this.initDate();
-  },
+const getBookHistory = async () => {
+  try {
+    const { data } = await axios.get(
+      `http://localhost:3000/api/v1/rooms/bookHistory/${props.id}`
+    );
+    console.log(data);
+    bookHistory.value = data.items;
+    // console.log(bookHistory.value);
+    bookHistory.value.map(book=>{
+
+   
+    const startDateParts = book.startDate.split(".");
+    const endDateParts = book.endDate.split(".");
+
+    const startDay = Number(startDateParts[0]);
+    const startMonth = Number(startDateParts[1]) - 1; // Months are zero-based
+    const startYear = Number(startDateParts[2]);
+    const startDate = new Date(startYear, startMonth, startDay);
+
+    const endDay = Number(endDateParts[0]);
+    const endMonth = Number(endDateParts[1]) - 1;
+    const endYear = Number(endDateParts[2]);
+    const endDate = new Date(endYear, endMonth, endDay);
+    // console.log(book.status === 1)
+    // console.log(book.status)
+    attributes.value.push(
+      {
+        highlight: book.status === 1 ? "red" : "green",
+        dates: [[startDate, endDate]],
+      });
+    });
+  } catch (error) {
+    console.error("Error getting room info:", error);
+  }
 };
+
+// Use watch to react to changes in info
+watch(info, (newInfo) => {
+  console.log("info changed:", newInfo);
+});
+
+onMounted(() => {
+  getInfo();
+});
 </script>
+
 <template>
   <div class="container">
     <header>
       <div class="backBut">
-        <button @click="goBack()">Назад</button>
+        <button @click="$router.go(-1)">Назад</button>
       </div>
       <div class="headerText text-center">
         {{ info.title }}
       </div>
     </header>
-    <div class="mainBody">
-      <VCalendar :columns="columns" :expanded="expanded">
-        <template #default="{ date }">
-          <div v-if="isCurrentMonth(date)">
-            {{ formatMonth(date) }}
-          </div>
-        </template>
-      </VCalendar>
-      <VDatePicker v-model="date" />
+    <div class="mainBody container text-center mt-4">
+      <VCalendar
+        :columns="columns"
+        :expanded="expanded"
+        :attributes="attributes"
+        @dayClick="handleDayClick"
+        locale="ru"
+      />
+      <VDatePicker v-model="date" locale="ru" />
+       <p v-if="selectedAttribute">Selected Attribute: {{ selectedAttribute.highlight }}</p> 
     </div>
+    
   </div>
 </template>
+
 <style lang="scss" scoped>
 .backBut {
   button {
